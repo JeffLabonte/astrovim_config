daccb1d278974890f87578514fc932ed0ce19e66
Jean-Francois Labonte <grimsleepless@protonmail.com>
2023-05-02 23:45:22 -0400

    Update the configurations for debugging

--- plugins/debugger.lua
+++ plugins/debugger.lua
@@ -3,16 +3,27 @@ return {
     "mfussenegger/nvim-dap-python",
     dependencies = {
       "mfussenegger/nvim-dap",
-      "Joakker/lua-json5",
-    },
-    config = function()
-      local dap_vscode = require("dap.ext.vscode")
-      dap_vscode.json_decode = require("json5").parse
-      dap_vscode.load_launchjs(nil, { debugpy = { "python" }, lldb = { "rust" } })
-    end,
+    }
   },
   {
-    'Joakker/lua-json5',
-    run = './install.sh'
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "theHamsta/nvim-dap-virtual-text",
+      "rcarriga/nvim-dap-ui",
+      "mfussenegger/nvim-dap-python",
+      "Pocco81/DAPInstall.nvim",
+    },
+    config = function()
+      local dap = require("dap")
+
+      dap.adapters.lldb = {
+        type = "executable",
+        command = "lldb-vscode",
+        name = "lldb",
+      }
+
+      local dap_vscode = require("dap.ext.vscode")
+      dap_vscode.load_launchjs(nil, { debugpy = { "python" }, lldb = { "rust" } })
+    end,
   }
 }
